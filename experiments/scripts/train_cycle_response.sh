dc4a211380ff658eb4110369fee2d06e8f1272e2
wenz116 <b02901109@ntu.edu.tw>
2018-11-11 22:34:28 +0800

    Upload training with response loss and caption loss

---
+++ experiments/scripts/train_cycle_response.sh
@@ -0,0 +1,36 @@
+
+
+GPU_ID=$1
+DATASET=$2
+SPLITBY=$3
+OUTPUT_POSTFIX=$4
+CAPTION_MODEL=$5
+CAPTION_LOSS_WEIGHT=$6
+
+IMDB="coco_minus_refer"
+ITERS=1250000
+TAG="notime"
+NET="res101"
+ID="mrcn_cmr_with_st"
+
+STEPSIZE="[360000]" ####
+ANCHORS="[4,8,16,32]"
+RATIOS="[0.5,1,2]"
+
+CUDA_VISIBLE_DEVICES=${GPU_ID} python ./tools/train_cycle_response.py \
+    --imdb_name ${IMDB} \
+    --net_name ${NET} \
+    --iters ${ITERS} \
+    --tag ${TAG} \
+    --dataset ${DATASET} \
+    --splitBy ${SPLITBY} \
+    --output_postfix ${OUTPUT_POSTFIX} \
+    --caption_model ${CAPTION_MODEL} \
+    --start_from caption_log_response \
+    --cap_loss_weight ${CAPTION_LOSS_WEIGHT} \
+    --max_iters 800000 \
+    --with_st 1 \
+    --id ${ID} \
+    --cfg experiments/cfgs/${NET}.yml \
+    --set ANCHOR_SCALES ${ANCHORS} ANCHOR_RATIOS ${RATIOS} \
+    TRAIN.STEPSIZE ${STEPSIZE} # ${EXTRA_ARGS}
